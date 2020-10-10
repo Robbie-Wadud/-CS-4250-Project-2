6994866f241960fd695f8b93e589c3650cad7bf0
Robbie-Wadud <amaan.wadud@gmail.com>
2020-10-10 06:58:41 -0500

    Program to remove the second element from a list

---
+++ Second Element Removed.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+;Defining the function
+(define (rem_second simpleList)
+  ;Check to see if the list exists or is empty
+  (if (list? simpleList) (if (null? simpleList) simpleList (if (eqv? (cdr simpleList) '()) '() (cons (car simpleList) (cddr simpleList)))) #f)
+)
+
+(rem_second '(A B C D E F))
+(rem_second '(1 2 3 4 5 6 7 8 9))
+(rem_second '())
+(rem_second 56)
+(rem_second '(A))
