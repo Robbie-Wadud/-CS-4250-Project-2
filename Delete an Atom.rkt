6813041c6b2b3824e88994483f241048e0c8a14d
Robbie-Wadud <amaan.wadud@gmail.com>
2020-10-10 19:48:50 -0500

    Program to delete an atom from a list

--- Delete an Atom.rkt
+++ Delete an Atom.rkt
@@ -1,6 +1,15 @@
 #lang racket
 ;Defining the function
 (define (my_delete atom myList)
-  (if (eqv? (car myList) atom) (cdr myList) #f)
+  (cond
+    ((null? myList) myList)
+    ((list? (car myList))
+     (cons (my_delete atom (car myList)) (my_delete atom (cdr myList))))
+    ((equal? atom (car myList)) (my_delete atom (cdr myList)))
+    (else (cons (car myList) (my_delete atom (cdr myList)))))
+  )
 
-)
+(my_delete 3 '(1 3 2 3))
+(my_delete 'A '(A B C D A E A F))
+(my_delete 'A '(A B (C A) R T (A J K A L) Z))
+(my_delete 'A '())
